84b8769589fb626696f836cd2b57941efe0f62c3
gouth7 <gsubram5@ncsu.edu>
2019-01-04 14:38:42 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-class App extends Component {
+export class MapContainer extends Component {
+
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+  });
+
+
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Map
+        google={this.props.google}
+        zoom={8}
+        onClick={this.onMapClicked}
+        initialCenter={{
+          lat: 35.787743,
+          lng: -78.64425
+        }}
+        >
+
+        <Marker onClick={this.onMarkerClick}
+                name={'School'}
+                position={{lat: 35.787743, lng: -78.644257}}
+         />
+
+         <InfoWindow
+           marker={this.state.activeMarker}
+           visible={this.state.showingInfoWindow}>
+             <div>
+               <h1>{this.state.selectedPlace.name}</h1>
+             </div>
+         </InfoWindow>
+
+      </Map>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCnY73kvyiQLtQwpli9k2GgD-G0dlAe8N0'
+})(MapContainer)
