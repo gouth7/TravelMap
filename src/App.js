2ffa877625eee651649554790c01ed70491c68b5
gouth7 <gsubram5@ncsu.edu>
2019-06-08 10:50:46 -0400

    module updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import ReactDOM from 'react-dom'
+import {Modal} from './Modal.js'
+import '../node_modules/react-modal-video/scss/modal-video.scss';
 
 export class MapContainer extends Component {
 
@@ -7,27 +10,40 @@ export class MapContainer extends Component {
     showingInfoWindow: false,
     activeMarker: {},
     selectedPlace: {},
+    modalState: false,
+    videoId: null,
   };
 
-  onMarkerClick = (props, marker, e) =>
+  onClose = () => this.setState({modalState: false});
+
+  onMarkerClick = (props) =>
     this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
+      modalState: true,
+      videoId: props.video
   });
 
+  onRightClick = (props, marker, e) => console.log('sdflj')
+    // this.setState({
+    //   selectedPlace: props,
+    //   activeMarker: marker,
+    //   showingInfoWindow: true,
+    // });
 
-  onMapClicked = (props) => {
+
+  onMapClicked = (props) =>
+  {
     if (this.state.showingInfoWindow) {
       this.setState({
         showingInfoWindow: false,
-        activeMarker: null
+        activeMarker: null,
+        modalState: false
       })
     }
   };
 
   render() {
     return (
+      <div>
       <Map
         google={this.props.google}
         zoom={8}
@@ -37,11 +53,37 @@ export class MapContainer extends Component {
           lng: -78.64425
         }}
         >
-
         <Marker onClick={this.onMarkerClick}
-                name={'School'}
-                position={{lat: 35.787743, lng: -78.644257}}
+                onMouseover={this.onRightClick}
+                name={'Mountains FB 18'}
+                position={{lat: 36.6212, lng: -81.4845}} //greyson highlands
+                video={'Vk_-KmWtbBE'}
+
          />
+         <Marker onClick={this.onMarkerClick}
+                 onMouseover={this.onRightClick}
+                 name={'Okeechobee SB 18'}
+                 position={{lat: 27.2439, lng: -80.8298}} //okee
+                 video={'w_pREDw12uE'}
+          />
+        <Marker onClick={this.onMarkerClick}
+                onMouseover={this.onRightClick}
+                name={'Singapore Summer 18'}
+                position={{lat: 1.3521, lng: 103.8198}} //Singapore
+                video={'mn820dv6bv0'}
+         />
+         <Marker onClick={this.onMarkerClick}
+                 onMouseover={this.onRightClick}
+                 name={'Singapore Summer 18'}
+                 position={{lat: 35.7688, lng: -78.6619}} //Dorthea Dix
+                 video={'sejVTQPwHnM'}
+          />
+          <Marker onClick={this.onMarkerClick}
+                  onMouseover={this.onRightClick}
+                  name={'NYC Dec 17'}
+                  position={{lat: 40.7128, lng: -74.0060}} //NYC
+                  video={'URCZsdvXF2s'}
+           />
 
          <InfoWindow
            marker={this.state.activeMarker}
@@ -50,8 +92,9 @@ export class MapContainer extends Component {
                <h1>{this.state.selectedPlace.name}</h1>
              </div>
          </InfoWindow>
-
       </Map>
+      <Modal videoId={this.state.videoId} onClose={this.onClose} isOpen={this.state.modalState}/>
+      </div>
     );
   }
 }
